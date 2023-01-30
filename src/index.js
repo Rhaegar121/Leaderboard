3699db3d1b43b965133aefc750d4826f4ecb9c3c
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-01-30 22:11:04 +0630

    add scores dynamically in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.css';
+
+const scores = document.querySelector('.scores');
+scores.innerHTML = `
+<li>Name: 100</li>
+<li>Name: 20</li>
+<li>Name: 50</li>
+<li>Name: 78</li>
+<li>Name: 125</li>
+<li>Name: 77</li>
+<li>Name: 42</li>`;
