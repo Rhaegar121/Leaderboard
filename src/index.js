9a7329259f3f8ce42eae5e7d99d2934f1d10a2e5
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-02-03 15:51:55 +0630

    add window.onload function and btn.disable function

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import postScore from './postScore.js';
 
 const form = document.querySelector('form');
 const refresh = document.querySelector('#refresh');
+const btn = document.querySelector('#submit');
+
+window.onload = () => {
+  getScore();
+};
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+  btn.disable = true;
   const formData = new FormData(e.target);
   const score = {
     user: formData.get('name'),
@@ -16,6 +22,7 @@ form.addEventListener('submit', (e) => {
     e.target.reset();
     getScore();
   });
+  btn.disable = false;
 });
 
 refresh.onclick = () => {
